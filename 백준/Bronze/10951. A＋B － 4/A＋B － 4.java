3873bec7cc211e4ac2a7e30957c23615d5e3c52a
Jiye-Park01 <109766173+Jiye-Park01@users.noreply.github.com>
2023-01-09 12:08:40 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String i;
+		StringTokenizer a;
+		while((i = bfr.readLine()) != null)
+		{
+			a = new StringTokenizer(i);
+//			bfw.write(Integer.parseInt(a.nextToken()) + Integer.parseInt(a.nextToken()));
+			System.out.println(Integer.parseInt(a.nextToken()) + Integer.parseInt(a.nextToken()));
+		}
+		bfr.close();
+		bfw.flush();
+		bfw.close();
+	}
+}
