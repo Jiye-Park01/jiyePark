f393ce3b7d03946bd8e5d3535c07324089451549
Jiye-Park01 <109766173+Jiye-Park01@users.noreply.github.com>
2023-01-11 20:02:40 +0900

    [Bronze III] Title: 최소, 최대, Time: 1708 ms, Memory: 318744 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] num = new int[N];
+
+		for(int i = 0; i < N; i++)
+			num[i] = sc.nextInt();
+		int M = num[0];
+		int m = num[0];
+		for(int i = 1; i < N; i++)
+		{
+			if(M < num[i])
+				M = num[i];
+			if(m > num[i])
+				m = num[i];
+		}
+		
+		System.out.println(m + " " + M);
+	}
+}
